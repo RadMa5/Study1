9ab17ec80ebf6a5e0da89dc0ff886e5c1abc0ec6
RadMa <maks25022@gmail.com>
2023-07-20 21:25:33 +0300

    A warm welcome

---
+++ Welcome/Welcome.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Please, enter your name: ");
+string name = Console.ReadLine();
+
+if(name.ToLower() == "max")
+{
+    Console.WriteLine("Welcome, Lord Max");
+}
+else
+{
+    Console.WriteLine("You suck");
+}
